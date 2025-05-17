9d99837ef7364cb9a40505af33401c880d3b7cdf
Praduman Kushwaha <pkstd1916@gmail.com>
2025-05-18 00:44:11 +0530

    change env

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
 const config = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT, // Add this line
   dialect: 'mysql',
+  dialectOptions: {
+    ssl: { // Required for Railway
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 };
 
 module.exports = config;
--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT, // Add this line
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  ssl: { // Required for Railway
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
